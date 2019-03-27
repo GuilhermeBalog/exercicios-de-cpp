56c06fe940050a1847e04bd8996f5b3aac403745
GuilhermeBalog <guibalog@gmail.com>
2019-03-27 16:59:29 -0300

    Exercício 1186, sobre matrizes, comparação e diagonal secundária

---
+++ uri/iniciante/1186-abaixo-da-diagonal-secundaria.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string operacao = "";
+	float matriz[12][12] = { 0.0 }, soma = 0;
+	int lado = 12;
+	int divisor = ((lado * lado) - lado) / 2;
+
+	cin >> operacao;
+	for (int i = 0; i < lado; i++) {
+		for (int j = 0; j < lado; j++) {
+			cin >> matriz[i][j];
+			if (i + j > (lado - 1)) {
+				soma += matriz[i][j];
+			}
+		}
+	}
+	cout.precision(1);
+	if (operacao == "S") {
+		cout << fixed << soma << endl;
+	}
+	else if (operacao == "M") {
+		cout << fixed << soma / divisor << endl;
+	}
+
+	return 0;
+}
